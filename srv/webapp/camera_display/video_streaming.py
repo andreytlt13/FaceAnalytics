f7f847bc6959eaf1373194a1a88b188c72a42f9a
andreytlt13 <andreytlt13@gmail.com>
2018-07-02 22:48:53 +0400

    add face detection (by haar scale detector)

--- srv/webapp/camera_display/video_streaming.py
+++ srv/webapp/camera_display/video_streaming.py
@@ -1,27 +1,46 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, Response
+import cv2
+import flask
 
-from srv.camera.opencv_read_stream import Camera
+CascadePath = 'haarcascade_face.xml'
+face_cascade = cv2.CascadeClassifier(CascadePath)
+video_capture = cv2.VideoCapture(0)
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return flask.render_template('index.html')
 
 
-def generate_stream(camera):
+def generate_stream():
+
     while True:
-        frame = camera.get_frame()
+        _, frame = video_capture.read()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30)
+        )
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        cv2.imwrite('frame.jpg', frame)
+
         yield (b'--frame\r\n'
-               b'Content-Type: image/bmp\r\n\r\n' + frame + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + open('frame.jpg', 'rb').read() + b'\r\n')
 
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(generate_stream(Camera('config.ini')),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    return flask.Response(generate_stream(),
+                          mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
