2d2d682ede3f9d5dc2cc18f0659fa543f9f6163d
Michael <mikhail.id.123@gmail.com>
2018-07-02 17:36:27 +0400

    Cleaning up
    * for now we need only one working implementation hence removing all the alternatives though we can come back to them later

--- srv/camera/async_recording/record_stream_on_disk_runer.py
+++
@@ -1,6 +0,0 @@
-from srv.camera.async_recording.record_stream_on_disk import CameraStreamOnDiskRecorder
-
-CONFIG_FILE_PATH = 'config.ini'
-
-reader = CameraStreamOnDiskRecorder(CONFIG_FILE_PATH)
-frames = reader.record_stream()
