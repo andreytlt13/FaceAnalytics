f6415e4e77153a639ad14aa95273a34615ff0d55
Viktor Zdanovich <viktor.zdanovich@cxense.com>
2018-09-21 14:20:04 +0400

    frontend initial commit

---
+++ frontend/src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    PageNotFoundComponent
+  ]
+})
+export class CoreModule { }
---
+++ frontend/src/app/material.module.ts
@@ -0,0 +1,9 @@
+// import * as All from '@angular/material';
+import {NgModule} from "@angular/core";
+
+@NgModule({
+  imports: [
+    // ...Object.keys(All).map(k=>All[k])
+  ]
+})
+export class MaterialModule{ }
