6455111e095ee539b9865ae0ebe52d32d7105fd6
Andrey Mescheryakov <andrey.mesheryakov@best4ad.com>
2018-12-04 20:41:14 +0400

    upgrade performance

--- srv/db/event.py
+++ srv/db/event.py
@@ -8,6 +8,20 @@ class Event(Base):
     __tablename__ = 'events_log'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
+    object_id = Column(Integer)
+    event_time = Column(BigInteger)
+    enter = Column(Integer)
+    exit = Column(Integer)
+
+    def __repr__(self):
+        return "<Event(id=%d, object_id='%d', event_time='%d', enter='%d', exit='%d')>" % (
+            self.id, self.object_id, self.event_time, self.enter, self.exit)
+
+
+class Event2(Base):
+    __tablename__ = 'events_log2'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
     person_id = Column(Integer)
     person_name = Column(String)
     age = Column(Integer)
--- srv/db/event_db_logger.py
+++ srv/db/event_db_logger.py
@@ -1,14 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from common import config_parser
+from db.event import Event, Base
 
-from srv.db.event import Event, Base
-
+CONFIG = config_parser.parse()
+DEFAULT_PATH = 'sqlite:///surveillance.db' #CONFIG['../db/db.sqlite3']
 Session = sessionmaker()
 
 
 class EventDBLogger:
 
-    def __init__(self, db_url) -> None:
+    def __init__(self, db_url=DEFAULT_PATH) -> None:
         engine = create_engine(db_url, echo=True)
         Base.metadata.create_all(engine)
         Session.configure(bind=engine)
