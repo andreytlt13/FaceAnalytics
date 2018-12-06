e3bdf957c55e6cc83ffcca17029ae67ff7f85d6b
Viktor Zdanovich <zdanovichviktor@mail.ru>
2018-12-06 20:36:12 +0400

    added creating cameras

--- frontend/src/app/dashboard/camera/camera.ts
+++ frontend/src/app/dashboard/camera/camera.ts
@@ -1,7 +1,16 @@
 export class Camera {
-  constructor(private id: string, public name: string, public url: string, public type = 'video') {}
+  constructor(public id: string, public name: string, public url: string, public type = 'video') {}
 
   static parse({ id, name, url, type }) {
     return new Camera(id, name, url, type);
+  }
+
+  toJSON() {
+    return {
+      id: this.id,
+      name: this.name,
+      url: this.url,
+      type: this.type
+    };
   }
 }
