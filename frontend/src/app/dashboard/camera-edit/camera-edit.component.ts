a121e858cddac61da11f72b5fe9b6aba14b56315
Viktor Zdanovich <zdanovichviktor@mail.ru>
2018-12-07 15:29:17 +0400

    separate camera edit page

--- frontend/src/app/dashboard/camera-edit/camera-edit.component.ts
+++ frontend/src/app/dashboard/camera-edit/camera-edit.component.ts
@@ -1,11 +1,4 @@
-import {Component, OnInit} from '@angular/core';
-import {Location} from '@angular/common';
-import {Camera} from '../camera/camera';
-import {Observable} from 'rxjs';
-import {DashboardState} from '../dashboard.state';
-import {Actions, ofActionDispatched, Store} from '@ngxs/store';
-import {CreateCamera, SelectCamera} from '../dashboard.actions';
-import {Router} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-camera-edit',
@@ -13,38 +6,10 @@ import {Router} from '@angular/router';
   styleUrls: ['./camera-edit.component.scss']
 })
 export class CameraEditComponent implements OnInit {
-  public selected$: Observable<Camera> = this.store.select(DashboardState.selectedCamera);
-  public camera = new Camera();
 
-  constructor(
-    private store: Store,
-    private router: Router,
-    private actions: Actions,
-    private location: Location
-  ) {
-  }
+  constructor() { }
 
   ngOnInit() {
-    this.selected$.subscribe((camera: Camera) => {
-      if (camera) {
-        this.camera = camera;
-      }
-    });
-
-    this.actions.pipe(ofActionDispatched(SelectCamera)).subscribe(({payload: {camera}}) => {
-      this.router.navigate(['dashboard', camera.id]);
-    });
-  }
-
-  saveCamera() {
-    console.log(this.camera);
-    if (Camera.isValid(this.camera)) {
-      this.store.dispatch(new CreateCamera({camera: this.camera}));
-    }
-  }
-
-  cancel() {
-    this.location.back();
   }
 
 }
