aa38a5053b4c9cef62565e56db2a04f2fc750a82
Viktor Zdanovich <zdanovichviktor@mail.ru>
2018-12-07 16:27:18 +0400

    added creating cameras

--- frontend/src/app/dashboard/camera-edit/camera-edit.component.ts
+++ frontend/src/app/dashboard/camera-edit/camera-edit.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
+import {tap} from 'rxjs/operators';
+import {Camera} from '../camera/camera';
+import {Observable} from 'rxjs';
+import {DashboardState} from '../dashboard.state';
+import {Store} from '@ngxs/store';
+import {CreateCamera} from '../dashboard.actions';
 
 @Component({
   selector: 'app-camera-edit',
@@ -6,10 +13,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./camera-edit.component.scss']
 })
 export class CameraEditComponent implements OnInit {
+  public selected$: Observable<Camera> = this.store.select(DashboardState.selectedCamera);
+  public camera = new Camera();
 
-  constructor() { }
+  constructor(
+    private store: Store,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    console.log(this.camera);
+    this.selected$.pipe(
+      tap((camera: Camera) => {
+        this.camera = camera;
+      })
+    );
+  }
+
+  saveCamera() {
+    if (Camera.isValid(this.camera)) {
+      this.store.dispatch(new CreateCamera({camera: this.camera}));
+    }
+  }
+
+  cancel() {
+    this.location.back();
   }
 
 }
