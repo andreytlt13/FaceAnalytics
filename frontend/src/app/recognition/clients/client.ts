6719540114cdcff50c9cae76e3f87a29a1170a7a
Viktor Zdanovich <zdanovichviktor@mail.ru>
2019-05-17 14:48:50 +0400

    prepared clients service

--- frontend/src/app/recognition/clients/client.ts
+++ frontend/src/app/recognition/clients/client.ts
@@ -1,12 +1,26 @@
+import {environment} from '../../../environments/environment';
+
 export class Client {
   constructor(
-    public id: string,
-    public photo: string,
-    public description: string,
+    public id: number,
+    public name?: string,
+    public description?: string,
     public rate?: number,
   ) {}
 
+  get photo() {
+    if (this.name) {
+      return `${environment.apiUrl}/get_face_known?name=${this.name}`;
+    }
+
+    if (this.id) {
+      return `${environment.apiUrl}/get_face?object_id=${this.id}`;
+    }
+
+    throw new Error('Both id and name are not set to the client');
+  }
+
   static parse(json: any) {
-    return new Client(json.id, json.photo, json.description, json.rate);
+    return new Client(json.id, json.name, json.description, json.rate);
   }
 }
--- frontend/src/app/recognition/clients/clients.service.ts
+++ frontend/src/app/recognition/clients/clients.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable, of} from 'rxjs';
-import {People} from './mock-clients';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
 import {Client} from './client';
 import {map} from 'rxjs/operators';
 
@@ -9,34 +8,49 @@ import {environment} from '../../../environments/environment';
 
 const CLIENTS_PATH = '';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class ClientsService {
   constructor(private readonly http: HttpClient) {}
   getAll(): Observable<any> {
-    return this.http.get(`${environment.apiUrl}/get_person_id`);
-  }
-
-  get(id: number): Observable<Client> {
-    return of({clients: [People.find(c => c.id === id)]})
+    return this.http.get(`${environment.apiUrl}/get_person_id`)
       .pipe(
-        map(({clients}) => Client.parse(clients[0]))
+        map(response => response)
       );
   }
 
-  map(client1: Client, client2: Client): Observable<any> {
+  getKnown(name: string): Observable<any> {
+    const params =  new HttpParams();
+
+    params.append('name', name);
+
+    return this.http.get(`${environment.apiUrl}/get_face_known`, {params})
+      .pipe(
+        map(response => response)
+      );
+  }
+
+  link(client1: Client, client2: Client): Observable<any> {
     return this.http.put(`${environment.apiUrl}/select_name`, {
-      object_id: 1,
-      name: 'cococo'
+      object_id: client1.id,
+      name: client2.name
     });
   }
 
-  rate(client: Client) {
-    return of(true);
+  getClientDescription(client: Client): Observable<any> {
+    const params = new HttpParams();
+
+    params.append('object_id', client.id.toString());
+
+    return this.http.get(`${environment.apiUrl}/get_description`, {params});
   }
 
-  update(client: Client) {
-    return of(true);
+  update(client: Client): Observable<any> {
+    return this.http.put(`${environment.apiUrl}/put_description`, {
+      description: client.description,
+      stars: client.rate ? client.rate : 0
+    });
   }
 }
